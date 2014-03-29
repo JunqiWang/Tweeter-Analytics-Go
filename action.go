813abdf5692fb03f26ef765a9d2f9a54d12931bc
Junqi Wang <junqi.wang@live.com>
2014-03-29 16:06:19 -0400

    Update action.go

--- action.go
+++ action.go
@@ -5,17 +5,12 @@ import (
     "time"
 )
 
-func index(val string) string {
-    return "Team Dynamos\nTwitter Analytics"
-}
-
 func hello(val string) string {
     return "Dynamos 0000-0000-0000\n" + 
 	    time.Now().Format("2006-01-02 15:04:05")
 } 
 
 func main() {
-    web.Get("/()", index)
     web.Get("/(q1)", hello)
     web.Run("0.0.0.0:9999")
 }
